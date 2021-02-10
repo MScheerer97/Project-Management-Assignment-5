6e0b8d07efbd56a14af77728d6c0858f64b8f3ad
Martin Scheerer <martin-scheerer97@web.de>
2021-02-10 18:06:39 +0100

    First try to solve task 4: for loop applied, need to check tomorrow.

--- Assignment 5 R Code.R
+++ Assignment 5 R Code.R
@@ -55,14 +55,25 @@ venue_data <- data.frame(name, city, postalCode, address, url, longitude, latitu
 glimpse(venue_data)
 
 #################################### Task 4
+# set base url and make request again to be sure
+root <-  "https://app.ticketmaster.com/discovery/v2/venues"
+get_it <- GET(url = root, query = list(apikey = .key, 
+                                       countryCode = "DE", 
+                                       locale = "*"))
+
+content <-  fromJSON(content(get_it, as = "text"))
 
 n <-  as.numeric(content$page[["totalElements"]])
-pages <- as.numeric(content$page[["totalPages"]])
+
+# adjust pages for for-loop later
+pages <- as.numeric(content$page[["totalPages"]])-1
 entries <- as.numeric(content$page[["size"]])
 
-full_pages <- floor(n/entries)
+entries_last_page <- n-entries*pages
 
-venues <- data.frame(
+# adjust full pages -1 for the for-loop
+
+venue_data <- data.frame(
   name = character(n),
   city = character(n),
   postalCode = character(n),
@@ -71,3 +82,43 @@ venues <- data.frame(
   longitude = character(n), 
   latitude = character(n)
 )
+
+# robust code for the loop: last page is not full; considered by applying 
+# if statement
+for(j in 0:pages){
+  if(j < pages){
+    get_it <- GET(url = root, query = list(apikey = .key, 
+                                           countryCode = "DE", 
+                                           locale = "*", 
+                                           page = j))
+    content <-  fromJSON(content(get_it, as = "text"))
+    
+    venue_data$name[j*entries+1:j*entries+entries] <- content[["_embedded"]][["venues"]]$name
+    venue_data$city[j*entries+1:j*entries+entries] <- content[["_embedded"]][["venues"]]$city$name
+    venue_data$postalCode[j*entries+1:j*entries+entries] <- content[["_embedded"]][["venues"]]$postalCode
+    venue_data$address[j*entries+1:j*entries+entries] <- content[["_embedded"]][["venues"]]$address$line1
+    venue_data$url[j*entries+1:j*entries+entries] <- content[["_embedded"]][["venues"]]$url
+    venue_data$longitude[j*entries+1:j*entries+entries] <- as.numeric(content[["_embedded"]][["venues"]]$location$longitude)
+    venue_data$latitude[j*entries+1:j*entries+entries] <- as.numeric(content[["_embedded"]][["venues"]]$location$latitude)
+    
+  }
+  else{
+    get_it <- GET(url = root, query = list(apikey = .key, 
+                                           countryCode = "DE", 
+                                           locale = "*", 
+                                           page = j))
+    content <-  fromJSON(content(get_it, as = "text"))
+    
+    venue_data$name[j*entries+1:entries_last_page] <- content[["_embedded"]][["venues"]]$name
+    venue_data$city[j*entries+1:entries_last_page] <- content[["_embedded"]][["venues"]]$city$name
+    venue_data$postalCode[j*entries+1:entries_last_page] <- content[["_embedded"]][["venues"]]$postalCode
+    venue_data$address[j*entries+1:entries_last_page] <- content[["_embedded"]][["venues"]]$address$line1
+    venue_data$url[j*entries+1:entries_last_page] <- content[["_embedded"]][["venues"]]$url
+    venue_data$longitude[j*entries+1:entries_last_page] <- as.numeric(content[["_embedded"]][["venues"]]$location$longitude)
+    venue_data$latitude[j*entries+1:entries_last_page] <- as.numeric(content[["_embedded"]][["venues"]]$location$latitude)
+  }
+
+}
+
+
+
